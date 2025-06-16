c48402b102f32a507ac559bfd5adf601b891ec97
PietPadda <pietergdewaal@gmail.com>
2025-06-16 17:58:58 +0200

    setup a basic server with HTML handler for webpage

--- main.go
+++ main.go
@@ -1,8 +1,49 @@
 // main.go
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	m := http.NewServeMux() // create new request router
+
+	m.HandleFunc("/", handlePage) // send requests for the root path endpoint to handlePage
+
+	const port = "8010" // no magic numbers, set port
+
+	// define the server struct
+	srv := http.Server{
+		Handler:      m,                // use router as handler
+		Addr:         ":" + port,       // :port
+		WriteTimeout: 30 * time.Second, // timeout for safety
+		ReadTimeout:  30 * time.Second, // timeout for safety
+	}
+	// writes to/reads from client after X time closes connection
+
+	// this blocks forever, until the server
+	// has an unrecoverable error
+	fmt.Println("server started on ", port) // start log
+	err := srv.ListenAndServe()             // server blocker -- infinite loop
+	log.Fatal(err)                          // only ends when fatal err returns
+}
+
+// our root path endpoint handler
+func handlePage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html") // set header to html
+	w.WriteHeader(200)                          // 200 OK status code
+
+	// simple html page
+	const page = `<html> 
+<head></head>
+<body>
+	<p> Hello from Docker! I'm a Go server. </p>
+</body>
+</html>
+`
+	// html response writer
+	w.Write([]byte(page)) // write html const page as byte slice for html response
 }
